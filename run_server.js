bc60a9661566b0cbb2b31b059ab03adc7aba0c16
Kawandeep Virdee <whichlight@gmail.com>
2013-11-14 01:26:50 -0500

    tried screen

--- run_server.js
+++ run_server.js
@@ -42,12 +42,20 @@ for(var i=0;i<bufferlen;i++){
 }
 
 var screen = new Array(RESOLUTION);
+
 for(var i=0;i<screen.length;i++){
   screen[i]=new Array(RESOLUTION);
 }
 
-//new idea- write to screen, stream that
+var clearScreen = function(){
+  for(var i=0;i<screen.length;i++){
+    for(var j=0;j<screen.length;j++){
+      screen[i][j]={"x":i, "y":j, "hue":0, "s":0, "life":1};
+    }
+  }
+}
 
+clearScreen();
 
 
 serialPort.on("open", function () {
@@ -64,23 +72,38 @@ serialPort.on("open", function () {
   eventEmitter.on('send_point_serial', function(p){
     var h = Math.floor(p.hue *1535);
     var str = p.x+','+p.y+','+h+','+Math.floor(p.s*255)+','+Math.floor(255)+';';
-    console.log(str);
     serialPort.write(str, function(err, res){
-      if (err) console.log('err '+err);
       console.log(str);
+      if (err) console.log('err '+err);
     });
   });
+
+
+  eventEmitter.on('send_array_serial', function(arr){
+    for(var i=0;i<screen.length;i++){
+      for(var j=0;j<screen.length;j++){
+        var p = screen[i][j];
+        var h = Math.floor(p.hue *1535);
+        var str = p.x+','+p.y+','+h+','+Math.floor(p.s*255)+','+Math.floor(255)+';';
+        serialPort.write(str, function(err, res){
+          if (err) console.log('err '+err);
+        });
+      }
+    }
+  });
+
 });
 
 //io to touch
 io.sockets.on('connection', function (socket) {
   socket.on('point', function (data) {
-    points.push(new Point(t.x, t.y, t.hue, 1,1));
+   eventEmitter.emit('send_point_serial', new Point(data.x, data.y, data.hue, 1,1));
     //buffer[bufferindex][t.x+RESOLUTION*t.y]=t;
   });
 });
 
 //from touch to points
+/*
 setInterval(function(){
 //  addBufferPoints(buffer[bufferindex]);
   points.forEach(function(point){
@@ -90,8 +113,8 @@ setInterval(function(){
   });
   bufferindex++;
   bufferindex%=bufferlen;
-
 }, 100);
+*/
 
 
 
